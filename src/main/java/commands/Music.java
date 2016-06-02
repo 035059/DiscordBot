d8103581a45129d8107dcecb97d64aa5d557884a
Allin Demopolis <allindemopolis@gmail.com>
2016-06-01 21:28:29 -0400

    music still in progress

--- src/main/java/commands/Music.java
+++ src/main/java/commands/Music.java
@@ -5,7 +5,6 @@ import main.java.ICommand;
 import sx.blah.discord.handle.obj.IMessage;
 import sx.blah.discord.handle.obj.IVoiceChannel;
 
-import java.net.URL;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -30,11 +29,13 @@ public final class Music implements ICommand {
         if (args[0].toLowerCase().equals("play") || args[0].equals("queue")){
             System.out.println("play/queue");
             try {
-                List<IVoiceChannel> channelsByName = GeneralCommands.client.getVoiceChannels().stream().filter(iChannel -> iChannel.getName().equals(args[1])).collect(Collectors.toList());
+                List<IVoiceChannel> channelsByName = GeneralCommands.client.getVoiceChannels().stream().filter(VoiceChannel -> VoiceChannel.getName().equals(args[1])).collect(Collectors.toList());
                 for (IVoiceChannel channel: channelsByName){
-                    queue(channel, new URL(args[2]));
-                }
+                    System.out.println(channel.getName());
 
+                    channel.join();
+                    channel.getAudioChannel().queueUrl(args[2]);
+                }
             } catch (Exception e) {
                 e.printStackTrace();
             }
@@ -42,40 +43,45 @@ public final class Music implements ICommand {
             System.out.println("resume");;
             try {
                 List<IVoiceChannel> channelsByName = GeneralCommands.client.getVoiceChannels().stream().filter(iChannel -> iChannel.getName().equals(args[1])).collect(Collectors.toList());
-                for (IVoiceChannel channel: channelsByName)
-                    toggleState(channel);
-
+                for (IVoiceChannel channel : channelsByName) {
+                    channel.join();
+                    Thread.sleep(10000);
+                    if (!channel.getAudioChannel().isPaused())
+                        channel.getAudioChannel().pause();
+                    else
+                        channel.getAudioChannel().resume();
+                }
             } catch (Exception e) {
                 e.printStackTrace();
             }
         } else if (args[0].toLowerCase().equals("stop")) {
             System.out.println("stop");
-            try {
-                List<IVoiceChannel> channelsByName = GeneralCommands.client.getVoiceChannels().stream().filter(iChannel -> iChannel.getName().equals(args[1])).collect(Collectors.toList());
-                for (IVoiceChannel channel: channelsByName)
-                    stop(channel);
+            if (args.length > 2) {
+                try {
+                    List<IVoiceChannel> channelsByName = GeneralCommands.client.getVoiceChannels().stream().filter(iChannel -> iChannel.getName().equals(args[1])).collect(Collectors.toList());
+                    for (IVoiceChannel channel : channelsByName) {
+                        channel.join();
+                        Thread.sleep(10000);
+                        channel.getAudioChannel().clearQueue();
+                        channel.leave();
+                    }
 
-            } catch (Exception e) {
-                e.printStackTrace();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            } else {
+                try {
+                    for (IVoiceChannel channel : GeneralCommands.client.getConnectedVoiceChannels()) {
+                        channel.join();
+                        Thread.sleep(10000);
+                        channel.getAudioChannel().clearQueue();
+                        channel.leave();
+                    }
+                } catch (Exception e){
+                    e.printStackTrace();
+                }
             }
         }
-    }
-
-    private void queue (IVoiceChannel channel, URL url)throws Exception{
-        channel.join();
-        channel.getAudioChannel().queueUrl(url);
-    }
-
-    private void toggleState(IVoiceChannel channel) throws Exception {
-        if (!channel.getAudioChannel().isPaused())
-            channel.getAudioChannel().pause();
-        else
-            channel.getAudioChannel().resume();
-    }
-
-    private void stop(IVoiceChannel channel) throws Exception {
-        channel.getAudioChannel().clearQueue();
-        channel.leave();
     }
 
     @Override
