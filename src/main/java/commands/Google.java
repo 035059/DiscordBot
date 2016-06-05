6c0ca2cb2a4a5bbcec3e5928ccd74b13067549bb
Allin Demopolis <allindemopolis@gmail.com>
2016-06-05 19:41:51 -0400

    Google Comments

--- src/main/java/commands/Google.java
+++ src/main/java/commands/Google.java
@@ -18,27 +18,42 @@ import java.util.Objects;
  */
 public final class Google implements ICommand {
 
+    /**
+     * Gets the name of the command
+     * @return the name of the command
+     */
     @Override
     public String getName() {
         return "google";
     }
 
+    /**
+     * Gets the role of the command
+     * @return the role of the command
+     */
     @Override
     public String getRole() {
         return "Posts a google search to the chat";
     }
 
+    /**
+     * Sends a message to the chat containing a google search of the inputted argument
+     * @param message the message that triggered the command
+     * @param args the space separated values that followed the command
+     */
     @Override
     public void handle(IMessage message, String[] args) {
-        if (Objects.equals(args[0], "LUCKY")) {
+        if (Objects.equals(args[0], "LUCKY")) { // If the first argument is LUCKY
             try {
-                args[0] = "";
+                args[0] = ""; // Set args[0] to an empty string
+                // Post a new message the the chat the trigger message came from with a link to the top google search result for the rest of the arguments
                 new MessageBuilder(GeneralCommands.client).withChannel(message.getChannel()).withContent(String.valueOf(new URL("http://www.google.com/search?q=" + Arrays.toString(args).replaceAll("\\[|\\]|,", "").replaceAll(" ", "+").substring(1) + "&btnI"))).build();
             } catch (MalformedURLException | DiscordException | MissingPermissionsException | HTTP429Exception e) {
                 e.printStackTrace();
             }
         } else {
             try {
+                // Post a new message the the chat the trigger message came from with a link to the google search results for the rest of the arguments
                 new MessageBuilder(GeneralCommands.client).withChannel(message.getChannel()).withContent(String.valueOf(new URL("http://www.google.com/search?q=" + Arrays.toString(args).replaceAll("\\[|\\]|,", "").replaceAll(" ", "+")))).build();
             } catch (MalformedURLException | DiscordException | MissingPermissionsException | HTTP429Exception e) {
                 e.printStackTrace();
@@ -46,6 +61,10 @@ public final class Google implements ICommand {
         }
     }
 
+    /**
+     * Gets the name and role of the command
+     * @return the name and role of the command
+     */
     @Override
     public String toString() {
         return this.getName() + ": " + this.getRole();
