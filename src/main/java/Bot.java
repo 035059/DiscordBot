9c475e27ce3d78eea392c694a5f6aad8e399b311
Allin Demopolis <allindemopolis@gmail.com>
2016-06-05 19:28:52 -0400

    Bot comments

--- src/main/java/Bot.java
+++ src/main/java/Bot.java
@@ -2,33 +2,35 @@ package main.java;
 
 import sx.blah.discord.api.ClientBuilder;
 import sx.blah.discord.api.IDiscordClient;
+import sx.blah.discord.handle.obj.Status;
 import sx.blah.discord.util.DiscordException;
-
-import java.util.Optional;
 
 
 public class Bot {
     public static IDiscordClient discordClient;
 
     /**
+     * Main function
      * @param args the token
      * @throws Exception
      */
     public static void main(String[] args) throws Exception {
-        if (args.length < 1)
-            System.out.print("You need to specify a token before continuing");
+        if (args.length < 1) // If no args are passed
+            System.out.print("You need to specify a token before continuing"); // Tell the user
 
-        discordClient = new ClientBuilder()
-                .withToken(args[0])
-                .login();
+        discordClient = new ClientBuilder() // Make a new client instance
+                .withToken(args[0]) // with the token passed in the build arguments
+                .login(); // and login
 
-        discordClient.getModuleLoader().loadModule(new GeneralCommands());
-        discordClient.getModuleLoader().loadModule(new CommandModule());
+        discordClient.getModuleLoader().loadModule(new GeneralCommands()); // Add the GeneralCommands Module
+        discordClient.getModuleLoader().loadModule(new CommandModule()); // Add the CommandModule Module
 
+        // Wait until the bit is fully connected
         while (!discordClient.isReady()) {
             Thread.sleep(100);
         }
-        discordClient.updatePresence(true, Optional.of("Discord4j"));
+
+        discordClient.changeStatus(Status.stream("Discord4J", "https://austinv11.github.io/Discord4J/")); // Change the bot's presence
     }
 
     /**
