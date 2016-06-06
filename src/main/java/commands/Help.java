df655f4350350a99c24727e614b10799a5effa58
Allin Demopolis <allindemopolis@gmail.com>
2016-06-05 20:31:55 -0400

    Horn and Help comments

--- src/main/java/commands/Help.java
+++ src/main/java/commands/Help.java
@@ -13,33 +13,49 @@ import sx.blah.discord.util.MissingPermissionsException;
  * Created by Allin on 5/26/2016.
  */
 public final class Help implements ICommand {
+    /**
+     * Get the name of the command
+     * @return the name of the command
+     */
     @Override
     public String getName() {
         return "help";
     }
 
+    /**
+     * Get the role of the command
+     * @return the role of the command
+     */
     @Override
     public String getRole() {
         return "posts a message with help for each function";
     }
 
+    /**
+     * Sends a message to the chat with a list of commands
+     * @param message the message that triggered the command
+     * @param args the space separated values that followed the command
+     */
     @Override
     public void handle(IMessage message, String[] args) {
-
-        StringBuilder stringBuilder = new StringBuilder();
-        stringBuilder.append("**List of commands:**\n");
-        for (ICommand s : CommandRegistry.getCommands().values()) {
-            stringBuilder.append("`" + s.getName() + "`: " + s.getRole());
-            stringBuilder.append("\n");
+        StringBuilder stringBuilder = new StringBuilder(); // MAke a new StringBuilder
+        stringBuilder.append("**List of commands:**\n"); // Append a title
+        for (ICommand s : CommandRegistry.getCommands().values()) { // For each command in the registry
+            stringBuilder.append("`").append(s.getName()).append("`: ").append(s.getRole()); // Append the command info
+            stringBuilder.append("\n"); // Append a line break
         }
-
         try {
+            // Send a message to the same chat as the trigger message with the String Builder as contents
             new MessageBuilder(GeneralCommands.client).withChannel(message.getChannel()).withContent(stringBuilder.toString()).build();
         } catch (HTTP429Exception | DiscordException | MissingPermissionsException e) {
             e.printStackTrace();
         }
     }
 
+    /**
+     * Get the name and role of the command
+     * @return the name and role of the command
+     */
     @Override
     public String toString() {
         return this.getName() + ": " + this.getRole();
--- src/main/java/commands/Horn.java
+++ src/main/java/commands/Horn.java
@@ -21,62 +21,78 @@ import java.util.Random;
  */
 public final class Horn implements ICommand {
 
-
+    /**
+     * Gets the name of the command
+     * @return the name of the command
+     */
     @Override
     public String getName() {
         return "horn";
     }
 
+    /**
+     * Gets the role of the command
+     * @return the role of the command
+     */
     @Override
     public String getRole() {
         return "plays a quick audio file";
     }
 
+    /**
+     * Plays a horn audio clip to the voice chat
+     * @param message the message that triggered the command
+     * @param args the space separated values that followed the command
+     */
     @Override
     public void handle(IMessage message, String[] args) {
-        File[] files = new File("C:\\Users\\Allin\\IdeaProjects\\DiscordBotGit\\src\\main\\java\\commands\\horns").listFiles();
+        File[] files = new File("C:\\Users\\Allin\\IdeaProjects\\DiscordBotGit\\src\\main\\java\\commands\\horns").listFiles(); // Get a list of available horns
         Random rd = new Random();
         IVoiceChannel channel = message.getAuthor().getConnectedVoiceChannels().get(0);
         assert files != null;
-        File file = files[rd.nextInt(files.length)];
+        File file = files[rd.nextInt(files.length)]; // Set the file to be played to a random file
         File temp = file;
 
-        if (args.length > 0) {
-            if (args[0].equals("horns")){
+        if (args.length > 0) { // If there are arguments
+            if (args[0].equals("horns")){ // If thr first argument is horns
                 StringBuilder sb = new StringBuilder();
-                sb.append("```");
-                for (File file1: files){
-                    sb.append(file1.getName().substring(0, file1.getName().length()-4));
-                    sb.append("\n");
+                sb.append("```"); // Append the opening tag for a code block
+                for (File file1: files){ // For each file in the list of files
+                    sb.append(file1.getName().substring(0, file1.getName().length()-4)); // Append the name of the file (strips the .mp3 at the end)
+                    sb.append("\n"); // Append a line break
                 }
-                sb.append("```");
+                sb.append("```");// Append the closing tag for a code block
                 try {
                     new MessageBuilder(GeneralCommands.client).withChannel(message.getChannel()).withContent(sb.toString()).build();
                 } catch (HTTP429Exception | DiscordException | MissingPermissionsException e) {
                     e.printStackTrace();
                 }
                 file = null;
-            } else {
-                System.out.println(Arrays.toString(args).replaceAll("\\]", "").replaceAll("\\[", "").replaceAll(",", ""));
-                for (File file1 : files) {
+            } else { // If the first argument is something else
+                for (File file1 : files) { // For each file in the list of files
+                    // If the name of the file is the same as the arguments
                     if (file1.getName().toLowerCase().contains(Arrays.toString(args).replaceAll("\\]", "").replaceAll("\\[", "").replaceAll(",", "")))
-                        file = file1;
+                        file = file1; // Set the file to be played to this file
                 }
             }
         }
 
-        if (!channel.isConnected())
-            channel.join();
+        if (!channel.isConnected()) // If we are not connected to the voice channel
+            channel.join(); // Connect
         try {
-            if (file != null) {
-                System.out.println(file.getName());
-            AudioPlayer.getAudioPlayerForGuild(message.getGuild()).queue(file);
+            if (file != null) { // If the file is not null
+                System.out.println(file.getName()); // Print the name of the file
+                AudioPlayer.getAudioPlayerForGuild(message.getGuild()).queue(file); // Queue the file in the audio player
             }
         } catch (IOException | UnsupportedAudioFileException e) {
             e.printStackTrace();
         }
     }
 
+    /**
+     * Gets the name and role of the command
+     * @return the name and role of the command
+     */
     @Override
     public String toString() {
         return this.getName() + ": " + this.getRole();
