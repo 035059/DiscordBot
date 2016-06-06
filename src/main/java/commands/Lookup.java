55e76ee6a41cd4a30d86a90199c34c4217aaa1de
Allin Demopolis <allindemopolis@gmail.com>
2016-06-05 20:38:31 -0400

    lookup comments

--- src/main/java/commands/Lookup.java
+++ src/main/java/commands/Lookup.java
@@ -8,22 +8,38 @@ import sx.blah.discord.handle.obj.IMessage;
  */
 public final class Lookup implements ICommand {
 
-
+    /***
+     * Gets the name of the command
+     * @return the name of the command
+     */
     @Override
     public String getName() {
         return "lookup";
     }
 
+    /***
+     * Gets the role of the command
+     * @return the role of the command
+     */
     @Override
     public String getRole() {
         return null;
     }
 
+    /**
+     * Searches the chat for a message
+     * @param message the message that triggered the command
+     * @param args the space separated values that followed the command
+     */
     @Override
     public void handle(IMessage message, String[] args) {
 
     }
 
+    /***
+     * Gets the name and role of the command
+     * @return the name and role of the command
+     */
     @Override
     public String toString() {
         return this.getName() + ": " + this.getRole();
