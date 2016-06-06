9d5ae4972e57ea6d1eef17b856d15823d0f46df5
Allin Demopolis <allindemopolis@gmail.com>
2016-06-05 20:37:08 -0400

    HYPE comments

--- src/main/java/commands/Hype.java
+++ src/main/java/commands/Hype.java
@@ -15,30 +15,52 @@ import sx.blah.discord.util.MissingPermissionsException;
 
 public class Hype implements ICommand {
 
+    /***
+     * Gets the name of the command
+     * @return the name of the command
+     */
     @Override
     public String getName() {
         return "hype";
     }
 
+    /***
+     * Gets the role of the command
+     * @return the role of the command
+     */
     @Override
     public String getRole() {
         return "For when the train simply cannot be stopped";
     }
 
+    /**\
+     * Gets whether or not the triggering message is deleted
+     * @return whether or not the triggering message is deleted
+     */
     @Override
     public boolean deletesMessage() {
         return true;
     }
 
+    /**
+     * Posts a hype message to the triggering messages channel
+     * @param message the message that triggered the command
+     * @param args the space separated values that followed the command
+     */
     @Override
     public void handle(IMessage message, String[] args) {
         try {
+            // Post a new message with HYPE!!!!!
             new MessageBuilder(GeneralCommands.client).withChannel(message.getChannel()).withContent("༼ つ ◕\\_◕ ༽つHYPE༼ つ ◕\\_◕ ༽つ").build();
         } catch (HTTP429Exception | MissingPermissionsException | DiscordException e) {
             Discord4J.LOGGER.error("Exception", e);
         }
     }
 
+    /***
+     * Gets the name and role of the command
+     * @return the name and role of the command
+     */
     @Override
     public String toString() {
         return this.getName() + ": " + this.getRole();
